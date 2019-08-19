dd338025b510e2270c0a3e2f7e77e0d6a72d7d9e
Denise Vissepo <7296322+8bitBabe@users.noreply.github.com>
2019-08-19 18:32:48 -0400

    Hello, World and enhancement
    Hello, World project that takes in name and returns date and time. Made following the Microsoft tutorial.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            //Prints out "Hello, World!" then pauses until the user presses a key.
+            //The program then closes. 
+            //Console.WriteLine("Hello, World!");
+            //Console.Write("Press any key to continue...");
+            //Console.ReadKey(true);
+
+
+            //Displays prompt for user's name, then returns the answer
+            //in an interpolated string (the "$" character)
+            //with current date and time from the DateTime.Now property
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+
+
         }
     }
 }
